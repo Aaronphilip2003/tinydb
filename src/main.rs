4a9e07530e491b474182cb25a19e0dbf60bdbbf4
Aaronphilip2003 <aaronphilip2003@gmail.com>
2026-05-19 22:02:55 +0530

    infinity loop to enter a command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
+// fn main() {
+//     let mut db = HashMap::new();
+//     db.insert(String::from("name"), String::from("Aaron"));
+//     db.insert(String::from("city"),String::from("Pune"));
+//     db.insert(String::from("language"),String::from("Rust"));
+//     println!("{:?}", db);
+// }
+
+// use std::collections::HashMap;
+use std::io;
 fn main() {
-    let mut db = HashMap::new();
-    db.insert(String::from("name"), String::from("Aaron"));
-    db.insert(String::from("city"),String::from("Pune"));
-    db.insert(String::from("language"),String::from("Rust"));
-    println!("{:?}", db);
+    // let mut db = HashMap::new();
+    loop {
+        let mut input = String::new();
+        println!("Enter a command");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        let input = input.trim();
+        println!("The input is {input}");
+    }
 }
